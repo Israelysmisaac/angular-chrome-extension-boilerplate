eead201678fbfed9662efb150113f468b1494460
WorkmatesDevOps <gitworkmates@gmail.com>
2022-07-29 16:00:21 +0530

    Revert "initial commit"
    This reverts commit 2b7cab7263099a5b9b021c35035ade657df40727.

--- custom-webpack.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Configuration } from 'webpack';
-
-module.exports = {
-  entry: { background: 'src/background/background.ts' },
-  optimization: {
-    runtimeChunk: false,
-  }
-} as Configuration;
